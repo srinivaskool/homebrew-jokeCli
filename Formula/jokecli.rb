da604d15e4e9ecea4ffff837565d85cf76a86f15
srini <srinivas_konduri@apple.com>
2023-02-17 22:36:06 +0530

    changes

--- Formula/jokecli.rb
+++ Formula/jokecli.rb
@@ -1,4 +1,4 @@
-class jokeCli < Formula
+class Jobcli < Formula
   desc "Get Human Readable file size information srini. - CLI"
   homepage "https://github.com/srinivaskool/jokeCli"
   url "https://github.com/srinivaskool/jokeCli/releases/download/v0.0.1/joke-cli-0.0.1.tgz"
