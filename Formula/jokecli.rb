a0642fb9a9e6673f2df236a454a0ee6da8414146
srini <srinivas_konduri@apple.com>
2023-02-17 22:26:34 +0530

    chnages done

---
+++ Formula/jokecli.rb
@@ -0,0 +1,12 @@
+class Getfilesize < Formula
+  desc "Get Human Readable file size information srini. - CLI"
+  homepage "https://github.com/srinivaskool/jokeCli"
+  url "https://github.com/srinivaskool/jokeCli/releases/download/v0.0.1/joke-cli-0.0.1.tgz"
+  sha256 "ab25bb7c9662735ffc843875dbf0f686fcc62ee68062815b3dec33e5809b8c3c"
+  license "MIT"
+  version "0.0.1"
+
+  def install
+    bin.install "joke-cli-0.0.1"
+  end
+end
