b3f29ec2870f2c9bb3fc6b82d1d073aa90755f11
srinivas.kool <31861607+srinivaskool@users.noreply.github.com>
2023-02-17 22:48:45 +0530

    Update jokecli.rb

--- Formula/jokecli.rb
+++ Formula/jokecli.rb
@@ -1,12 +1,12 @@
 class Jokecli < Formula
   desc "Get Human Readable file size information srini. - CLI"
   homepage "https://github.com/srinivaskool/jokeCli"
-  url "https://github.com/srinivaskool/jokeCli/releases/download/v0.0.1/joke-cli-0.0.1.tgz"
-  sha256 "ab25bb7c9662735ffc843875dbf0f686fcc62ee68062815b3dec33e5809b8c3c"
+  url "https://github.com/srinivaskool/jokeCli/releases/download/v0.0.1/jokecli.tgz"
+  sha256 "6c8cb7855e01cb38566bec1606a826c67d4c89bedce75ed15a3841571242662d"
   license "MIT"
   version "0.0.1"
 
   def install
-    bin.install "Jokecli"
+    bin.install "jokecli"
   end
 end
